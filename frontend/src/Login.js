97edd572edf1fad90af40623f683b8c2c82536b1
HunainMulla <mullahunain222in@gmail.com>
2025-02-07 16:38:57 +0530

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
+
+import React, { useState,useContext } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { correct } from './Components/loginSlice';
 import './ComponentsStyle/FormStyle.css';
-import { setname } from './Components/userDetailSlice';
-
+import { setname, setUserDetailFromLocalStorage } from './Components/userDetailSlice';
+import { UserContext } from './App';
 const Login = () => {
   const dispatch = useDispatch();
+
   const navigate = useNavigate();
 
   const [freelancerEmail, setFreelancerEmail] = useState('');
   const [freelancerPass, setFreelancerPass] = useState('');
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
-
+  
   const handleFreelancerSubmit = async (event) => {
     event.preventDefault();
-
+    
     let data = {
       email: freelancerEmail,
       password: freelancerPass,
     };
-
+    dispatch(setname(freelancerEmail))
     try {
       const response = await fetch("http://localhost:5000/form/signin", {
         method: "POST",
@@ -34,8 +36,11 @@ const Login = () => {
       if (response.ok) {
         const result = await response.json();
         console.log(result.message);
-        dispatch(setname(userEmail));
+
+
+  
         dispatch(correct());
+        // dispatch(setUserDetailFromLocalStorage());
         navigate('/');
       } else {
         console.log("Failed to login");
@@ -45,36 +50,7 @@ const Login = () => {
     }
   };
 
-  const handleCustomerSubmit = async (event) => {
-    event.preventDefault();
 
-    let data = {
-      email: userEmail,
-      password:userPassword,
-    };
-
-    try {
-      const response = await fetch("http://localhost:5000/form/signin", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (response.ok) {
-        dispatch(setname(userEmail));
-        const result = await response.json();
-        console.log(result.message);
-        dispatch(correct());
-        navigate('/');
-      } else {
-        alert("Invalid credentials!");
-      }
-    } catch (error) {
-      console.error("Error during login:", error);
-    }
-  };
 
   return (
     <div className="login-wrapper">
